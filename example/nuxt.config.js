8b38ae0c7606d3eeb13895b4ea0dfc4c6699a653
lihbr <lihbr@users.noreply.github.com>
2021-03-10 17:02:14 +0100

    test: remove test

--- example/nuxt.config.js
+++ example/nuxt.config.js
@@ -3,16 +3,13 @@ export default {
     '../src/module.ts'
   ],
   ackee: {
-    server: 'https://ackee.lihbr.com',
-    domainId: '6383a4e0-d3fb-4612-aee2-09fbe47e94d7',
+    server: '',
+    domainId: '',
     detailed: 'opt-in',
     ignoreLocalhost: false,
     ignoreOwnVisits: false
   },
   publicRuntimeConfig: {
-    ackeeTestEvent: '7b9515fd-bbdc-4e25-a8ba-04679297c380'
-  },
-  server: {
-    host: '192.168.1.16'
+    ackeeTestEvent: ''
   }
 }
